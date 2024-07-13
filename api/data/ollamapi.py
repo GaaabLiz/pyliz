43a1a1a78fbfddf2510a4753610d4e872b3c535a
gabliz <lizzosgabriele@gmail.com>
2024-07-13 10:29:42 +0200

    started ollama creation

--- api/data/ollamapi.py
+++ api/data/ollamapi.py
@@ -2,49 +2,62 @@ import base64
 
 import requests
 
+from network.netres import NetResponse
+from network.netutils import exec_get, exec_post
 
 OLLAMA_PORT = "11434"
 OLLAMA_HTTP_LOCALHOST_URL = "http://localhost:" + OLLAMA_PORT
 
+# https://github.com/ollama/ollama-python
+# https://github.com/ollama/ollama/blob/main/docs/api.md
 
-def check_ollama_status(url) -> NetResponse:
-    return exec_get(url)
+class OllamaApiLegacy:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def check_ollama_status(url) -> NetResponse:
+        return exec_get(url)
+
+    @staticmethod
+    def get_installed_models(url) -> NetResponse:
+        api_url = url + "/api/tags"
+        return exec_get(api_url)
+
+    @staticmethod
+    def send_query(
+            url: str,
+            prompt: str,
+            model_name: str,
+    ) -> NetResponse:
+        api_url = url + "/api/generate"
+        payload = {
+            "model": model_name,
+            "prompt": prompt,
+            "stream": False
+        }
+        return exec_post(api_url, payload, False)
+
+    @staticmethod
+    def send_llava_query(
+            url: str,
+            prompt: str,
+            image_base_64: str,
+            model_name: str,
+    ) -> NetResponse:
+        api_url = url + "/api/generate"
+        payload = {
+            "model": model_name,
+            "prompt": prompt,
+            "images": [image_base_64],
+            "format": "json",
+            "stream": False
+        }
+        return exec_post(api_url, payload, False)
 
 
-def get_installed_models(url) -> NetResponse:
-    api_url = url + "/api/tags"
-    return exec_get(api_url)
+class Ollamapi:
 
-
-def send_query(
-        url: str,
-        prompt: str,
-        model_name: str,
-) -> NetResponse:
-    api_url = url + "/api/generate"
-    payload = {
-        "model": model_name,
-        "prompt": prompt,
-        "stream": False
-    }
-    return exec_post(api_url, payload, False)
-
-
-def send_llava_query(
-        url: str,
-        prompt: str,
-        image_base_64: str,
-        model_name: str,
-) -> NetResponse:
-    api_url = url + "/api/generate"
-    payload = {
-        "model": model_name,
-        "prompt": prompt,
-        "images": [image_base_64],
-        "format": "json",
-        "stream": False
-    }
-    return exec_post(api_url, payload, False)
-
-
-
+    def __init__(self, url: str):
+        self.url = url
+        pass
