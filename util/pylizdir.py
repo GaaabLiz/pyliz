1af1c1c63b558bac61a79c857afa007c544d61bb
gabliz <lizzosgabriele@gmail.com>
2024-10-20 19:00:34 +0200

    updated pylizdir.py

--- util/pylizdir.py
+++ util/pylizdir.py
@@ -1,12 +1,13 @@
 import os
+from typing import List
 
 from util import pathutils
 from util.cfgutils import CfgItem, Cfgini
 
 
 class PylizDir:
-
-    path: str = pathutils.get_app_home_dir(".pyliz")
+    cfgini: Cfgini = None
+    path: str = None
     path_config_ini = os.path.join(path, "config.ini")
 
     default_path_models: str = os.path.join(path, "models")
@@ -16,14 +17,16 @@ class PylizDir:
     ]
 
     @staticmethod
-    def create():
+    def create(folder_name: str):
+        # Settaggio path
+        PylizDir.path = pathutils.get_app_home_dir(folder_name)
         # Cartella pyliz
         pathutils.check_path(PylizDir.path, True)
         pathutils.check_path_dir(PylizDir.path)
         # File config.ini
-        cfgini = Cfgini(PylizDir.path_config_ini)
-        if not cfgini.exists():
-            cfgini.create(PylizDir.ini_items_list)
+        PylizDir.cfgini = Cfgini(PylizDir.path_config_ini)
+        if not PylizDir.cfgini.exists():
+            PylizDir.cfgini.create(PylizDir.ini_items_list)
         # Cartella models
         pathutils.check_path(PylizDir.default_path_models, True)
         pathutils.check_path_dir(PylizDir.default_path_models)
@@ -51,3 +54,11 @@ class PylizDir:
         path = os.path.join(PylizDir.path, "logs")
         pathutils.check_path(path, True)
         return path
+
+    @staticmethod
+    def set_ini_item(item: CfgItem):
+        PylizDir.cfgini.write(item.section, item.key, item.value)
+
+    @staticmethod
+    def get_ini_item(section: str, key: str, is_bool=False):
+        return PylizDir.cfgini.read(section, key, is_bool)
