3e701d899b9bd162989636a935e640d43c0f48f3
gabliz <lizzosgabriele@gmail.com>
2024-10-21 21:57:48 +0200

    update some data model files

--- ai/core/ai_source_type.py
+++ ai/core/ai_source_type.py
@@ -2,9 +2,10 @@ from enum import Enum
 
 
 class AiSourceType(Enum):
-    OLLAMA_SERVER = "Ollama Remote",
-    LMSTUDIO_SERVER = "LMStudio Remote",
-    # LOCAL_AI = "Local AI"
-    LOCAL_LLAMACPP = "Local Llamacpp",
-    LOCAL_LLAMACPP_LIB = "Local Llamacpp Library"
-    API_MISTRAL = "API Mistral"
+    OLLAMA_SERVER = "Ollama server",
+    LMSTUDIO_SERVER = "LMM studio server",
+    LOCAL_LLAMACPP = "Local (Llamacpp)",
+    LOCAL_LLAMACPP_LIB = "Local (Official Llamacpp Library)"
+    API_MISTRAL = "Mistral API"
+    GEMINI = "Gemini"
+
