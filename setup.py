807fef2ab228d2e48936b4b69b091501a7e7ea8b
gabliz <lizzosgabriele@gmail.com>
2024-11-17 17:27:14 +0100

    migrated media files to another git repo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 DESCRIPTION = 'Personal python library for general purposes.'
 LONG_DESCRIPTION = 'A personal python library for general purposes.'
 
@@ -21,7 +21,9 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+
+    ],
     keywords=['python', 'video', 'utilities'],
     classifiers=[
         "Development Status :: 1 - Planning",
