c25ae437b5a5a5fba15d6886cbde7318716212e1
gabliz <lizzosgabriele@gmail.com>
2025-01-04 14:59:09 +0100

    updated logging logic

--- pylizlib/log/pylizLogging.py
+++ pylizlib/log/pylizLogging.py
@@ -1,7 +1,10 @@
 
 
 # log_config.py
-from loguru import logger
+from loguru import logger as base_logger
+
+# Crea un logger specifico per A
+logger = base_logger.bind(library="PylizLib")
 
 # Disattiva tutti i log all'inizio
 logger.remove()
@@ -9,14 +12,14 @@ logger.remove()
 
 
 def enable_logging(level="DEBUG", file_path=None, to_stdout=True):
-    """Abilita il logging con il livello e il percorso file opzionali."""
+    """Abilita il logging con il livello e il percorso file opzionali per A."""
 
     # Log su file
     if file_path:
         logger.add(
             file_path,
             level=level,
-            format="{time} {level} {message}",
+            format="{time} {level} {extra[library]} {message}",
             rotation="10 MB",
             compression="zip",
             serialize=False
@@ -27,15 +30,15 @@ def enable_logging(level="DEBUG", file_path=None, to_stdout=True):
         logger.add(
             lambda msg: print(msg, end=""),  # Stampare direttamente a stdout
             level=level,
-            format="{time:HH:mm:ss} {level} {message}"
+            format="{time:HH:mm:ss} {level} {extra[library]} {message}"
         )
 
-    logger.info("Logging abilitato per la libreria.")
+    logger.info("Logging abilitato per la libreria PylizLib.")
 
 
 def test_logging():
-    logger.debug("This is a debug message")
-    logger.info("This is an info message")
-    logger.warning("This is a warning message")
-    logger.error("This is an error message")
-    logger.critical("This is a critical message")
+    logger.debug("This is a debug message from PylizLib")
+    logger.info("This is an info message from PylizLib")
+    logger.warning("This is a warning message from PylizLib")
+    logger.error("This is an error message from PylizLib")
+    logger.critical("This is a critical message from PylizLib")
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 DESCRIPTION = 'Personal python library for general purposes.'
 LONG_DESCRIPTION = 'A personal python library for general purposes.'
 
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     install_requires=[
-        "requests", "loguru",
+        "requests",
     ],
     keywords=['python', 'video', 'utilities'],
     classifiers=[
