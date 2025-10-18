27cb376a36ce0b070e93c54862a01515f703fe1b
Gabriele <lizzosgabriele@gmail.com>
2025-10-18 18:40:59 +0200

    feat: add QtFwQConfigGroup dataclass for configuration grouping

--- pylizlib/qtfw/domain/setting.py
+++ pylizlib/qtfw/domain/setting.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from qfluentwidgets.common import ConfigItem
 
 
@@ -6,3 +8,10 @@ class QtFwQConfigItem(ConfigItem):
     def __init__(self, enabled: bool, group, name, default, validator=None, serializer=None, restart=False):
         super().__init__(group, name, default, validator, serializer, restart)
         self.enabled = enabled
+
+
+@dataclass
+class QtFwQConfigGroup:
+    name: str
+    description: str = ""
+    enabled: bool = True
