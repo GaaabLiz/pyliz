a74b56f3ac78ed2d6a94bceb8d65bd00941fc2cc
gabliz <lizzosgabriele@gmail.com>
2026-02-12 18:01:04 +0100

    build: add pyliz-media configuration to project.mk

--- project.mk
+++ project.mk
@@ -9,7 +9,7 @@ INNO_SETUP_VERSION_VARIABLE := MyAppVersion
 # == APPLICATIONS CONFIGURATION ==
 
 # Define the list of EXEs to build
-APPS_LIST := pyliz qtliz
+APPS_LIST := pyliz qtliz pyliz-media
 
 # Configuration for 'pyliz'
 pyliz_NAME := pyliz
@@ -22,6 +22,12 @@ qtliz_NAME := qtliz
 qtliz_MAIN := $(PYTHON_MAIN_PACKAGE)/qt/cli.py
 qtliz_ICO := $(pyliz_ICO)
 qtliz_ICNS := $(pyliz_ICNS)
+
+# Configuration for pyliz-media
+pyliz_media_NAME := pyliz-media
+pyliz_media_MAIN := $(PYTHON_MAIN_PACKAGE)/media/cli.py
+pyliz_media_ICO := $(pyliz_ICO)
+pyliz_media_ICNS := $(pyliz_ICNS)
 
 
 # == FILES VARIABLES ==
