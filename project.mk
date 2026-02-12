5a262d1ad62ea43f568286514f794b7d1dd23a41
gabliz <lizzosgabriele@gmail.com>
2026-02-12 18:47:50 +0100

    build: rename pyliz-media application configuration in project.mk

--- project.mk
+++ project.mk
@@ -9,7 +9,7 @@ INNO_SETUP_VERSION_VARIABLE := MyAppVersion
 # == APPLICATIONS CONFIGURATION ==
 
 # Define the list of EXEs to build
-APPS_LIST := pyliz qtliz pylizmedia
+APPS_LIST := pyliz qtliz pyliz_media
 
 # Configuration for 'pyliz'
 pyliz_NAME := pyliz
@@ -24,7 +24,7 @@ qtliz_ICO := $(pyliz_ICO)
 qtliz_ICNS := $(pyliz_ICNS)
 
 # Configuration for pyliz-media
-pyliz_media_NAME := pylizmedia
+pyliz_media_NAME := pyliz-media
 pyliz_media_MAIN := $(PYTHON_MAIN_PACKAGE)/media/cli.py
 pyliz_media_ICO := $(pyliz_ICO)
 pyliz_media_ICNS := $(pyliz_ICNS)
