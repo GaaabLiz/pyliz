044232dad7bc895693198ab9970f0b7991937caf
gabliz <lizzosgabriele@gmail.com>
2026-02-13 10:50:46 +0100

    fix: enable exif date extraction for all media types in organizer

--- pylizlib/media/script/organizer/media_org.py
+++ pylizlib/media/script/organizer/media_org.py
@@ -177,7 +177,7 @@ class MediaOrganizer:
         Extracts creation date details from EXIF or filesystem.
         Returns: (year, month, day, timestamp)
         """
-        if self.options.exif and media_item.is_image:
+        if self.options.exif:
             creation_date = media_item.creation_date_from_exif_or_file_or_sidecar
             return creation_date.year, creation_date.month, creation_date.day, creation_date.timestamp()
         else:
