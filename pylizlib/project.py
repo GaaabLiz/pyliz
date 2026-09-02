2879604589b5ab5b957093434f2a2b558ee8827d
gabliz <lizzosgabriele@gmail.com>
2026-09-02 19:05:08 +0200

    bump: v0.6.11

--- pylizlib/project.py
+++ pylizlib/project.py
@@ -1,6 +1,6 @@
 # fmt: off
 name = 'pylizlib'
-version = '0.6.10'
+version = '0.6.11'
 description = 'Add your description here'
 requires_python = '>=3.12'
 authors = [('Gabliz', 'gabliz.dev@gmail.com')]
